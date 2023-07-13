e5c8e3b89496d966693d9e61ae252c84b814d64b
aqyuki <yksk171118@gmail.com>
2023-07-13 21:15:29 +0900

    :fix: fix output

--- print.go
+++ print.go
@@ -16,5 +16,5 @@ func ErrorPrint(message string) {
 }
 
 func printWithPrefix(message string, prefix Prefix) {
-	fmt.Printf("%s%c%s\n", message, Space, prefix.Get())
+	fmt.Printf("%s%c%s\n", prefix.Get(), Space, message)
 }
