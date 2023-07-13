4ddcfd9af008bb549db196c5e327c01e3a4c63c1
aqyuki <yksk171118@gmail.com>
2023-07-13 21:22:56 +0900

    :test: add new test

--- print_test.go
+++ print_test.go
@@ -35,3 +35,32 @@ func TestErrorPrint(t *testing.T) {
 		})
 	}
 }
+func TestInfoPrint(t *testing.T) {
+
+	type args struct {
+		message string
+	}
+
+	tests := []struct {
+		name string
+		args args
+		want string
+	}{
+		{
+			name: "case 1",
+			args: args{
+				message: "sample",
+			},
+			want: "[Info] sample",
+		},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := pprint.PickStdout(t, func() { pprint.InfoPrint(tt.args.message) })
+			if got != tt.want {
+				t.Errorf("output : %v , want : %v\n", got, tt.want)
+			}
+		})
+	}
+}
