cf51281a1f00072415fd49e7f3eea03c4db84e4d
aqyuki <yksk171118@gmail.com>
2023-07-13 20:34:39 +0900

    ommit Messages ==== :add: add new type

--- default_prefix.go
+++ default_prefix.go
@@ -1,1 +1,21 @@
 package pprint
+
+type (
+	DefaultPrefix struct {
+		Message string
+	}
+)
+
+func (p DefaultPrefix) Get() string {
+	return p.Message
+}
+
+func (p DefaultPrefix) RegisterWord(words ...string) {}
+
+func (p DefaultPrefix) SetFormat(format string) {}
+
+func New(word string) *DefaultPrefix {
+	return &DefaultPrefix{
+		Message: word,
+	}
+}
