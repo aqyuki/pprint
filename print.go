e8062a1685d614a49ef62bf7d9bc7562f86b76c3
aqyuki <yksk171118@gmail.com>
2023-07-13 21:22:36 +0900

    :feat: add InfoPrint

--- print.go
+++ print.go
@@ -1,17 +1,27 @@
 package pprint
 
-import "fmt"
+import (
+	"fmt"
+)
 
 const (
 	// Space is char that space
 	Space = 0x20
 	// Error is prefix that error message
 	Error = "Error"
+	// Info is prefix that info message
+	Info = "Info"
 )
 
 // ErrorPrint print message with error prefix
 func ErrorPrint(message string) {
 	prefix := New(Error)
+	printWithPrefix(message, prefix)
+}
+
+// InfoPrint print message with information prefix
+func InfoPrint(message string) {
+	prefix := New("Info")
 	printWithPrefix(message, prefix)
 }
 
