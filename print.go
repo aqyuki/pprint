e265585076853378ed782ba4eeae1c7ee8d53b62
aqyuki <yksk171118@gmail.com>
2023-07-13 20:56:13 +0900

    :feat: add new feature to print with prefix

--- print.go
+++ print.go
@@ -1,1 +1,20 @@
 package pprint
+
+import "fmt"
+
+const (
+	// Space is char that space
+	Space = 0x20
+	// Error is prefix that error message
+	Error = "Error"
+)
+
+// ErrorPrint print message with error prefix
+func ErrorPrint(message string) {
+	prefix := New(Error)
+	printWithPrefix(message, prefix)
+}
+
+func printWithPrefix(message string, prefix Prefix) {
+	fmt.Printf("%s%s%s\n", message, string(Space), prefix.Get())
+}
