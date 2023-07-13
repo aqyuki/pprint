3bcc5fb956ae1a369ab566e9d172f032c0ff5951
aqyuki <yksk171118@gmail.com>
2023-07-14 08:33:22 +0900

    :fire: remove unused code

--- prefix.go
+++ prefix.go
@@ -4,12 +4,5 @@ type (
 	Prefix interface {
 		// Get return registered prefix
 		Get() string
-		// RegisterWord register new prefix
-		RegisterWord(words ...string)
-		// SetFormat set prefix format
-		// Args :
-		//  - format string
-		// 		- give format string
-		SetFormat(format string)
 	}
 )
